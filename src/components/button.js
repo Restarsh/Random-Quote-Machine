04729de3f05b6861c05587591bc217e2da7a1cc8
Amir Shrestha <amirshrestha2006@gmail.com>
2026-01-06 18:32:02 +0545

    remove button.js as it has no use

--- src/components/button.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Button = function({ buttonDisplayName, clickHandler }) {
-  return <button onClick={clickHandler}>{buttonDisplayName}</button>;
-};
-
-export default Button;
